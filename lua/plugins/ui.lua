7adad5e52d6628e9b13d6b3ae7238b2346315172
Matt Hagner <matthagner@gmail.com>
2023-05-29 21:07:00 -0500

    Add grapple.nvim
    I want to try this plugin out for tagging files while exploring new
    codebases.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -38,6 +38,17 @@ return {
     "nvim-lualine/lualine.nvim",
     config = function()
       require("lualine").setup({
+        sections = {
+          lualine_b = {
+            {
+              function()
+                local key = require("grapple").key()
+                return "  [" .. key .. "]"
+              end,
+              cond = require("grapple").exists,
+            },
+          },
+        },
         options = {
           theme = "auto",
         },
