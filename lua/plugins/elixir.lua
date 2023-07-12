ae5da9b542389922f6b73da0f0450da56ad08c1b
Matt Hagner <matthagner@gmail.com>
2023-07-12 07:44:31 -0500

    Start using elixir-tools.nvim
    This plugin handles setting up the language server and adds enhanced snippet
    support which is very nice while working in Elixir.

--- lua/plugins/elixir.lua
+++ lua/plugins/elixir.lua
@@ -2,6 +2,34 @@ if os.getenv("NVIM_ENABLE_ELIXIR") == "1" then
   return {
     { "elixir-editors/vim-elixir" },
     { "brendalf/mix.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+    {
+      "elixir-tools/elixir-tools.nvim",
+      version = "*",
+      event = { "BufReadPre", "BufNewFile" },
+      dependencies = {
+        "hrsh7th/nvim-cmp",
+      },
+      config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        local elixir = require("elixir")
+        local elixirls = require("elixir.elixirls")
+
+        elixir.setup({
+          credo = { enable = true },
+          elixirls = {
+            capabilities = capabilities,
+            enable = true,
+            settings = elixirls.settings({
+              dialyzerEnabled = false,
+              fetchDeps = true,
+              enableTestLenses = true,
+              suggestSpecs = true,
+            }),
+          },
+        })
+      end,
+    },
   }
 end
 
