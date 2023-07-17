f9cbd668c7825bb013c89875e1ac434ec7c100dc
Matt Hagner <matthagner@gmail.com>
2023-07-16 21:49:17 -0500

    Add keymap for cycling through buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set(
   "<C-\\><C-n>",
   { desc = "Remaps the way to enter normal mode when in the Neovim terminal" }
 )
+
+vim.keymap.set("n", "[b", "<CMD>bp<CR>", { desc = "Goes to the previous buffer" })
+vim.keymap.set("n", "]b", "<CMD>bp<CR>", { desc = "Goes to the next buffer" })
