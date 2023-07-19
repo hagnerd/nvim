7603faa21c6e9de5b8703f194613df447a69566f
Matt Hagner <matthagner@gmail.com>
2023-07-19 15:13:36 -0500

    Fix formatting
    For some reason, this file was incorrectly formatted previously

--- lua/util/lsp/init.lua
+++ lua/util/lsp/init.lua
@@ -4,13 +4,13 @@ local Lsp = {}
 --- and a buffer and leverages LspAttach
 ---@param on_attach fun(client: lsp.Client, buffer: Buffer):nil
 function Lsp.on_attach(on_attach)
-	vim.api.nvim_create_autocmd("LspAttach", {
-		callback = function(args)
-			local buffer = args.buf
-			local client = vim.lsp.get_client_by_id(args.data.client_id)
-			on_attach(client, buffer)
-		end,
-	})
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local buffer = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      on_attach(client, buffer)
+    end,
+  })
 end
 
 return Lsp
--- lua/util/lsp/keymaps.lua
+++ lua/util/lsp/keymaps.lua
@@ -2,22 +2,23 @@ local Keymaps = {}
 
 local opts = { noremap = true, silent = true, buffer = 0 }
 function Keymaps.on_attach(_, bufnr)
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-	vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, opts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, opts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, opts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, opts)
 
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-	vim.keymap.set("n", "<localleader>ff", function()
-		vim.lsp.buf.format({ async = true })
-	end, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "<localleader>ff", function()
+    vim.lsp.buf.format({ async = true })
+  end, opts)
 end
 
 return Keymaps
